bc04236e5e33aec64521b1d8299b2beacc6e9aa1
darrenteo17 <darrenteojunli@gmail.com>
2025-12-24 03:05:35 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -7,20 +7,14 @@ const urlsToCache = [
   "./manifest.json"
 ];
 
-// Install service worker and cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch files from cache if offline
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
