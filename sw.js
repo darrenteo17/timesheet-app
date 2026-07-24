363953e3225427f573222ee6316059ee727e83c6
darrenteo17 <darrenteojunli@gmail.com>
2026-07-25 02:03:06 +0800

    Enhance service worker with icon caching and activation
    Added icons to cache and implemented cache activation.

--- sw.js
+++ sw.js
@@ -4,15 +4,30 @@ const urlsToCache = [
   "./index.html",
   "./style.css",
   "./script.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install event: cache all files
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate event: remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+      )
+    )
+  );
+});
+
+// Fetch event: serve cached files first
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
